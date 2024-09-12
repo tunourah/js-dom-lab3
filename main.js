69bde2478d6b85d2dbda59d67e53d81e5e678aac
noura <noura.tu19@gmail.com>
2024-09-12 10:20:19 +0300

    solve lab

---
+++ main.js
@@ -0,0 +1,9 @@
+buttonOne = document.getElementById('button1');
+buttonTwo = document.getElementById('button2');
+buttonThree = document.getElementById('button3');
+
+
+buttonOne.classList.add('buttonone');
+buttonTwo.classList.add('buttontwo');
+
+buttonThree.classList.add('buttonthree');
